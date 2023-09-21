761215218e21274d575139d9c94307a812499d8b
Soumya Prakash Sahu <52891015+SOUMYA-CODING@users.noreply.github.com>
2023-09-21 19:41:37 +0530

    Implemented emailjs

--- components/Contact/contact-form.jsx
+++ components/Contact/contact-form.jsx
@@ -1,64 +1,48 @@
 "use client";
 
 import toast, { Toaster } from "react-hot-toast";
-import Button from "../ui/button"
-import { useState } from "react";
+import React, { useRef, useState } from "react";
+import emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
 
-    const [data, setData] = useState({
-        name: "",
-        email: "",
-        message: "",
-    })
+    const form = useRef();
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setData({
-            ...data,
-            [name]: value,
-        });
-    };
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
 
-    const sendEmail = async (e) => {
+    const sendEmail = (e) => {
         e.preventDefault();
-        const response = await fetch('/api/send', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        })
 
-        if (response.status === 200) {
-            setData({
-                name: "",
-                email: "",
-                message: "",
+        emailjs.sendForm(process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID, process.env.NEXT_PUBLIC_EMAIL_TEMPLATE, form.current, process.env.NEXT_PUBLIC_EMAIL_PUBLIC)
+            .then((result) => {
+                setName("");
+                setEmail("");
+                setMessage("");
+                toast.success("Successfully send the message")
+            }, (error) => {
+                toast.error("Something went wrong!")
             });
-            toast.success(`Hey ${data.name}, your message was sent successfully!`);
-        } else {
-            toast.error(`Something went wrong!`)
-        }
-    }
+    };
 
     return (
         <>
             <div className={`flex flex-col items-center justify-center`}>
-                <form onSubmit={sendEmail} className={`w-[50%] xl:w-[60%] md:w-full `} autoComplete="off">
+                <form ref={form} onSubmit={sendEmail} className={`w-[50%] xl:w-[60%] md:w-full `} autoComplete="off">
                     <div className={`flex flex-col items-start justify-start mb-8`}>
                         <label htmlFor="name">Name</label>
-                        <input type="text" value={data.name} onChange={handleChange} id="name" className={`border-b-2 bg-transparent border-black outline-none p-2 w-full`} required />
+                        <input type="text" id="name" name="user_name" value={name} onChange={(e) => setName(e.target.value)} className={`border-b-2 bg-transparent border-black outline-none p-2 w-full`} required />
                     </div>
                     <div className={`flex flex-col items-start justify-start mb-8`}>
                         <label htmlFor="email">E-mail</label>
-                        <input type="email" value={data.email} onChange={handleChange} id="email" className={`border-b-2 bg-transparent border-black outline-none p-2 w-full`} required />
+                        <input type="email" id="email" name="user_email" value={email} onChange={(e) => setEmail(e.target.value)} className={`border-b-2 bg-transparent border-black outline-none p-2 w-full`} required />
                     </div>
                     <div className={`flex flex-col items-start justify-start mb-8`}>
                         <label htmlFor="message">Message</label>
-                        <textarea name="" value={data.message} onChange={handleChange} id="message" cols="5" rows="4" className={`border-b-2 bg-transparent border-black outline-none p-2 w-full`} required></textarea>
+                        <textarea id="message" name="message" value={message} onChange={(e) => setMessage(e.target.value)} cols="5" rows="4" className={`border-b-2 bg-transparent border-black outline-none p-2 w-full`} required></textarea>
                     </div>
-                    <Button title="Send" onSubmit={sendEmail} />
+                    <input type="submit" value="Send" className={`relative  -top-1 -left-1 bg-primary py-2.5 px-6 text-lg font-semibold text-white transition-all before:absolute before:top-1 before:left-1 before:-z-[1] before:h-full before:w-full before:border-2 before:border-primary before:transition-all before:content-[''] hover:top-0 hover:left-0 before:hover:top-0 before:hover:left-0 md:p-2 md:px-4 md:text-base`} />
                 </form>
                 <Toaster position="bottom-right" toastOptions={{ duration: 3000 }} />
             </div>
