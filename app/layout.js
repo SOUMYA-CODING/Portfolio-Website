71d566a873841e778c426b1712cf6e8ce874a5e3
Soumya Prakash Sahu <52891015+SOUMYA-CODING@users.noreply.github.com>
2023-09-21 22:51:05 +0530

    removed layzloading

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,8 @@
-"use client";
-
 import Footer from '@/components/Footer/footer'
 import '../styles/global.css'
 import { Poppins } from 'next/font/google'
 import Navbar from '@/components/Navbar/navbar'
 import Icons from '@/components/Icons/icons'
-import { LazyMotion, domAnimation } from 'framer-motion'
 
 const font = Poppins({ subsets: ['latin', 'devanagari', 'latin-ext'], weight: ['100', '200', '300', '500', '600', '700', '800', '900'] })
 
@@ -18,14 +15,12 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className={font.className}>
-                <LazyMotion features={domAnimation}>
-                    <Navbar />
-                    <main>
-                        {children}
-                    </main>
-                    <Icons />
-                    <Footer />
-                </LazyMotion>
+                <Navbar />
+                <main>
+                    {children}
+                </main>
+                <Icons />
+                <Footer />
             </body>
         </html>
     )
